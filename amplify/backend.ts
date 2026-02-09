0725ac66c28095ae4e443a81a7e1485ddffe9636
Carl Higgs <carlhiggs@gmail.com>
2026-02-09 19:04:43 +1100

    updated lambda function set up for processing policy reports

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,17 +2,25 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { storage } from './storage/resource';
-import { processReportFunctionHandler } from './functions/process-policy-report/resource';
 import { PolicyStatement, Effect } from 'aws-cdk-lib/aws-iam';
 import { S3EventSource } from 'aws-cdk-lib/aws-lambda-event-sources';
 import { EventType } from 'aws-cdk-lib/aws-s3';
+import * as path from "node:path";
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { fileURLToPath } from "node:url";
+import * as iam from 'aws-cdk-lib/aws-iam';
+import { defineFunction } from "@aws-amplify/backend";
+import { CfnOutput, DockerImage, Duration } from "aws-cdk-lib";
+import { Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
+import { Bucket } from "aws-cdk-lib/aws-s3";
 
 const backend = defineBackend({
   auth,
   data,
   storage,
-  processReportFunctionHandler,
 });
+
+const customResourceStack = backend.createStack('GHSCIPolicyStack');
 
 // Add S3 permissions using wildcard to avoid circular dependency
 backend.auth.resources.authenticatedUserIamRole.addToPrincipalPolicy(
@@ -23,28 +31,37 @@ backend.auth.resources.authenticatedUserIamRole.addToPrincipalPolicy(
   })
 );
 
-// Grant Lambda permissions to access S3 bucket
-const s3Bucket = backend.storage.resources.bucket;
-s3Bucket.grantReadWrite(backend.processReportFunctionHandler.resources.lambda);
-
-// Grant Lambda permissions to update DynamoDB table
-backend.data.resources.tables['PolicyReport'].grantReadWriteData(
-  backend.processReportFunctionHandler.resources.lambda
+const processReportFunctionHandler = new lambda.Function(customResourceStack, 'process-policy-report', {
+      handler: "handler.handler",
+      runtime: Runtime.PYTHON_3_13,
+      timeout: Duration.seconds(300),
+      code: Code.fromAsset('amplify/functions/process-policy-report', {
+        bundling: {
+          image: DockerImage.fromRegistry("python:3.13-slim"),
+          command: [
+            '/bin/sh', '-c',
+            'pip install -r requirements.txt -t /asset-output && cp *.py /asset-output/'
+          ],
+          user: 'root',
+        },
+      }),
+      environment: {
+        STORAGE_BUCKET: backend.storage.resources.bucket.bucketName,
+        POLICY_REPORT_TABLE: backend.data.resources.tables['PolicyReport'].tableName,
+      },
+    }
 );
 
-// Add environment variables to Lambda
-backend.processReportFunctionHandler.resources.lambda.addEnvironment(
-  'STORAGE_BUCKET',
-  s3Bucket.bucketName
-);
-backend.processReportFunctionHandler.resources.lambda.addEnvironment(
-  'POLICY_REPORT_TABLE',
-  backend.data.resources.tables['PolicyReport'].tableName
-);
+processReportFunctionHandler.addPermission('AllowAuthenticatedUserInvoke', {
+  principal: new iam.ServicePrincipal('lambda.amazonaws.com'),
+  action: 'lambda:InvokeFunction',
+  sourceArn: backend.auth.resources.authenticatedUserIamRole.roleArn,
+})
 
 // Set up S3 event notification to trigger Lambda on .xlsx uploads
-backend.processReportFunctionHandler.resources.lambda.addEventSource(
-  new S3EventSource(s3Bucket, {
+processReportFunctionHandler.addEventSource(
+  new S3EventSource( 
+    backend.storage.resources.bucket as Bucket, {
     events: [EventType.OBJECT_CREATED],
     filters: [{ suffix: '.xlsx' }],
   })
--- amplify/functions/process-policy-report/resource.ts
+++ amplify/functions/process-policy-report/resource.ts
@@ -1,10 +1,8 @@
-import { execSync } from "node:child_process";
 import * as path from "node:path";
 import { fileURLToPath } from "node:url";
 import { defineFunction } from "@aws-amplify/backend";
 import { DockerImage, Duration } from "aws-cdk-lib";
 import { Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
-import * as fs from "fs";
 
 const functionDir = path.dirname(fileURLToPath(import.meta.url));
 
